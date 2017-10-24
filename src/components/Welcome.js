cf088a5bf3cadb623d467a56a95c7429a57ef7b4
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-10-24 17:01:08 -0400

    styled welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,10 +11,10 @@ class Welcome extends React.Component {
       <div className="welcome">
         <Navbar/>
         <div id="welcome-header">
-          <h1>Slam Dunk</h1>
-          <h2>The latest highlights from across the NBA</h2>
+          <h1 id="welcome-h1">Slam Dunk</h1>
+          <h2 id="welcome-h2">The latest highlights from across the NBA</h2>
           <br></br>
-          <Button onClick={() => this.props.history.push('/highlights')}>Highlights</Button>
+          <Button size='huge' onClick={() => this.props.history.push('/highlights')}>Highlights</Button>
         </div>
       </div>
     )
