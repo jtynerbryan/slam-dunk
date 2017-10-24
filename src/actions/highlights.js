0568309c806f7a4222723221328fc28510762ee3
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-10-24 18:19:37 -0400

    changed fetch url to heroku backend

--- src/actions/highlights.js
+++ src/actions/highlights.js
@@ -1,6 +1,6 @@
 export function getHighlights() {
   return (dispatch) => {
-    return fetch('http://localhost:3000/api/v1/get_highlights')
+    return fetch('https://slam-dunk-api.herokuapp.com/api/v1/get_highlights')
     .then(res => res.json())
     .then(res => {
       dispatch({ type: "GET_HIGHLIGHTS", payload: res.highlights })
