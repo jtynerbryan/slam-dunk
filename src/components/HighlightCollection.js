da5f2c45cc80e1887c61d05169ac5b8e33500f71
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-11-13 14:33:26 -0500

    fixed height of iframes, responsive for smaller screens, added semantic Divider for each highlight

--- src/components/HighlightCollection.js
+++ src/components/HighlightCollection.js
@@ -6,7 +6,7 @@ import ReactHtmlParser from 'react-html-parser';
 import Navbar from './Navbar'
 import Loader from './Loader'
 import LazyLoad from 'react-lazy-load';
-import { Button } from 'semantic-ui-react'
+import { Button, Divider } from 'semantic-ui-react'
 
 class HighlightCollection extends React.Component {
 
@@ -20,14 +20,16 @@ class HighlightCollection extends React.Component {
     if (this.props.highlights.length > 0) {
       const highlights = this.props.highlights.map((highlight, index) => {
         return (
-          <LazyLoad height={510} offsetVertical={900} key={index}>
-            <div>
+          <LazyLoad height={500} offsetVertical={900} key={index}>
+            <div className="highlight-container">
               <h3>{highlight.title} ({highlight.date_created.slice(0,10)})</h3>
               { ReactHtmlParser(ReactHtmlParser(highlight.media))}
               <div>
                 <Button href={highlight.permalink} target="_blank">Reddit Post</Button>
                 <Button href={highlight.url} target="_blank">Video Source</Button>
+                <Divider/>
               </div>
+
             </div>
           </LazyLoad>
         )
