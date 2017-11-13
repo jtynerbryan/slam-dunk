c635ba98986ac32a1e3a7d8b483f091516967a0d
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-11-13 15:35:15 -0500

    filtered highlights receiving the same styling as latest highlights

--- src/components/FilteredHighlightCollection.js
+++ src/components/FilteredHighlightCollection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import ReactHtmlParser from 'react-html-parser';
 import LazyLoad from 'react-lazy-load';
-import { Button } from 'semantic-ui-react'
+import { Button, Divider } from 'semantic-ui-react'
 
 class FilteredHighlightCollection extends React.Component {
 
@@ -10,13 +10,14 @@ class FilteredHighlightCollection extends React.Component {
     if (this.props.filteredHighlights.length > 0) {
       const highlights = this.props.filteredHighlights.map((highlight, index) => {
         return (
-          <LazyLoad height={510} offsetVertical={800} key={index}>
-            <div>
+          <LazyLoad height={400} offsetVertical={900} key={index}>
+            <div className="highlight-container">
               <h3>{highlight.title} ({highlight.date_created.slice(0,10)})</h3>
               { ReactHtmlParser(ReactHtmlParser(highlight.media))}
               <div>
                 <Button href={highlight.permalink} target="_blank">Reddit Post</Button>
                 <Button href={highlight.url} target="_blank">Video Source</Button>
+                <Divider/>
               </div>
             </div>
           </LazyLoad>
